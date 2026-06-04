b855072b198f10145c2c95bbdeb18f651c901d40
alumno <50525461@ivt.edu.ar>
2026-06-04 10:37:33 -0300

    4 de junio

---
+++ TP5/app.js
@@ -0,0 +1,14 @@
+let numero1= 5
+let numero2= 12
+let suma
+
+suma = numero1 + numero2
+let resta
+resta = numero1 - numero2
+
+console.log('La suma es: ' + suma)
+
+let nombre = 'Juana'
+let apellido = 'Scelzi'
+
+console.log(nombre + ' ' + apellido)
