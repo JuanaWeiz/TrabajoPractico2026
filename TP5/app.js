2e987d32ba2b17467439839f81161a8490868833
alumno <50525461@ivt.edu.ar>
2026-06-11 10:42:29 -0300

    11 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -4,10 +4,13 @@ let numero2= 12
 let suma
 suma = numero1 + numero2
 console.log('La suma es: ' + suma)
+alert('La suma es: ' + suma)
+
 
 let resta
 resta = numero2 - numero1
 console.log('La resta es: ' + resta)
+alert('La resta es:  ' + resta)
 
 let multiplicacion 
 multiplicacion = numero2 * numero1
@@ -16,9 +19,9 @@ console.log('La multiplicacion es: ' + multiplicacion)
 let division
 division = numero2 / numero1
 console.log('La division es: ' + division)
-alert('La division es: ' + division)
 
 let nombre = 'Juana'
 let apellido = 'Weiz'
+let edad = 14 
+console.log ('La alumna'+ ' ' + nombre + ' ' + apellido + ' tiene' +  ' ' + edad + ' ' + 'años')
 
-console.log(nombre + ' ' + apellido)
