cf860cb0d961a3e772d8568d13e806c1e21b973a
JuanaWeiz <50525461@ivt.edu.ar>
2026-07-30 10:15:07 -0300

    30/07

---
+++ TP6/app.js
@@ -0,0 +1,40 @@
+let b1= document.querySelector('#b1')
+let b2= document.querySelector('#b2')
+let b3= document.querySelector('#b3')
+let b4= document.querySelector('#b4')
+let b5= document.querySelector('#b5')
+let b6= document.querySelector('#b6')
+let p= document.querySelector('p')
+
+b1.onclick = function() {
+p.textContent='Este parrafo esta modificado'
+
+}
+
+
+b2.onclick = function() {
+p.style.backgroundColor= 'black'
+}
+    
+
+b3.onclick = function() {
+p.style.color='white'
+}
+
+        
+b4.onclick = function() {
+p.style.fontFamily= 'impact'
+}
+
+
+b5.onclick = function() {
+    p.style.fontSize = '60px'
+}
+    
+b6.onclick = function() {
+ p.style.color = 'darkred'
+ p.style.backgroundColor = 'rgb(248, 227, 230)'
+ p.textContent = 'esto es un parrafo'
+ p.style.fontFamily = 'Arial, Helvetica, sans-serif'
+ p.style.fontSize = '20px'
+}
